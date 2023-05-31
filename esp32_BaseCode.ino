a3b0c69e821a89372e971106aa427b43a3df5a98
Maks <maksimbalashov08@gmail.com>
2023-05-31 15:44:03 +0700

    AccessPoint_GetParam

--- esp32_BaseCode.ino
+++ esp32_BaseCode.ino
@@ -1,57 +1,63 @@
-#include <Arduino.h>
 #include <WiFi.h>
-#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
-extern "C"
-{
-#include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
-}
- 
-#define WIFI_SSID "TP-Link_F12C" // указывается к какой сети wifi нужно подключатся
-#define WIFI_PASSWORD "12138739" //  указывается пароль для выбранной сети wifi
 
-TimerHandle_t wifiReconnectTimer; // таймер подключения к wifi
- 
 AsyncWebServer server(80);
 
-void connectToWifi() // функция подключающая к wifi
-{
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // подключится к указанной сети wifi с указанным паролем
+String inputMessage;
+String inputParam;
+
+const char *ssid = "TEST-123";
+const char *password = NULL; // "12345678";
+
+const char* PARAM_INPUT_1 = "input1";
+
+IPAddress apIP(192, 168, 1, 4);
+const char *server_name = "www.myesp32.com";  // Can be "*" to all DNS requests
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <button onClick="window.location.reload();">Refresh Page</button>
+  <form action="/get">
+    input1: <input type="text" name="input1">
+    <input type="submit" value="Submit">
+  </form><br>
+</body></html>)rawliteral";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  delay(100);
+  
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+
+  const byte DNS_PORT = 53;
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      Serial.println(inputMessage);
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+                                     + inputParam + ") with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.begin();
 }
 
-void WiFiEvent(WiFiEvent_t event) // функция проверяющая подключение к wifi
-{
-  Serial.printf("[WiFi-event] event: %d\n", event);
-  switch (event) // проверяет какой отклик получен от wifi
-  {
-  case SYSTEM_EVENT_STA_GOT_IP: // ивент получаемый при успешном подключении wifi
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    break;
-  case SYSTEM_EVENT_STA_DISCONNECTED: // ивент получаемый при провальном подключении wifi
-    Serial.println("WiFi lost connection");
-    xTimerStart(wifiReconnectTimer, 0); // запустить таймер wifi
-    break;
-  }
-}
- 
-void setup(void) {
-  Serial.begin(115200);
-  WiFi.onEvent(WiFiEvent);
- 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "Hi! I am ESP32.");
-  });
- 
-  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
-  server.begin();
-  Serial.println("HTTP server started");
-}
- 
-void loop(void) {
-  AsyncElegantOTA.loop();
+void loop() {
+
 }
